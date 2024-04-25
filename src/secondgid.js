cb521fd0f71c07c96165d1b4fe5231d43b12a354
Vaibhavkulshrestha12 <vaibhavkulshrestha55@gmail.com>
2024-04-25 10:08:28 +0530

    ous

--- src/secondgid.js
+++ src/secondgid.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ImageCollage = () => {
     return (
-        // Main container div
+        
         <div className="flex h-screen w-full">
-            {/* Left half with a single image */}
+          
             <div className="w-1/2 h-full">
                 <img
                     src="https://www.ikea.com/ext/ingkadam/m/24c983181c6e7c59/original/PH187010-crop002.jpg?f=xl"
@@ -13,9 +13,8 @@ const ImageCollage = () => {
                 />
             </div>
 
-            {/* Right half with a flex column of 4 images */}
             <div className="w-1/4 h-full flex flex-col mx-2 ">
-                {/* First image */}
+               
                 <div className="w-[350px] h-[600px] m-2">
             <img
                 src="https://www.ikea.com/ext/ingkadam/m/1427af785fe647b8/original/PH181137-crop001.jpg?f=xs"
@@ -24,7 +23,7 @@ const ImageCollage = () => {
             />
         </div>  
 
-                {/* Second image */}
+               
                 <div className="w-[350px] h-[200px] m-2">
             <img
                 src="https://www.ikea.com/ext/ingkadam/m/1427af785fe647b8/original/PH181137-crop001.jpg?f=xs"
@@ -36,7 +35,7 @@ const ImageCollage = () => {
                 
             </div>
             <div className="w-1/4 h-full flex flex-col m-2">
-                {/* First image */}
+                
                 <div className="w-[350px] h-[400px] m-2">
             <img
                 src="https://www.ikea.com/ext/ingkadam/m/1427af785fe647b8/original/PH181137-crop001.jpg?f=xs"
@@ -44,7 +43,7 @@ const ImageCollage = () => {
                 className="w-full h-full object-cover"
             />
         </div>
-                {/* Second image */}
+                
                 <div className="w-[350px] h-[480px] m-2">
             <img
                 src="https://www.ikea.com/ext/ingkadam/m/1427af785fe647b8/original/PH181137-crop001.jpg?f=xs"
